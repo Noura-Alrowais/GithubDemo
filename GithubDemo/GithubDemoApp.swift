deff753fff490f8a8d93cef8502380324548dca2
Noura <nalrowais24@twq.idserve.net>
2024-09-26 11:49:14 +0300

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Noura Alrowais on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
